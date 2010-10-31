833b393148ca73165b6865bf0ac5cebc7e18be21
Sławek <sbogutyn@gmail.com>
2010-11-01 00:10:00 +0100

    pierwsza wrzutka

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forum::Application.config.secret_token = 'fecfbf3fe2936972c831070edca40b9fd2b97810b32814adc55f65a2e03332bd60b1daa511306edf819388354d2919fea8535bea6fb6c5f0768a900ab2cc9e31'
